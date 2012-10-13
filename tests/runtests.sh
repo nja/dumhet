08ea3fc1da7ee60ac1a64faf586b68ebb7ad50e3
Johan Andersson <nilsjohanandersson@gmail.com>
2012-10-13 22:22:11 +0200

    Truncate tests log file on each run

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,16 +1,20 @@
+TESTS_LOG=tests/tests.log
+
 echo "Running unit tests:"
+
+echo -n > ${TESTS_LOG}
 
 for i in tests/*_tests
 do
     if test -f $i
     then
-	if $VALGRIND ./$i 2>> tests/tests.log
+	if $VALGRIND ./$i 2>> ${TESTS_LOG}
 	then
 	    echo $i PASS
 	else
-	    echo "ERROR in test $i: here's tests/tests.log"
+	    echo "ERROR in test $i: here's ${TESTS_LOG}"
 	    echo "------"
-	    tail tests/tests.log
+	    tail ${TESTS_LOG}
 	    exit 1
 	fi
     fi
