30b7013657b21e338793825c2e08fb38e11bffd8
vagrant <vagrant@vagrantc.(none)>
2013-02-04 08:08:04 +0000

    Portable ignoring of unused argc.

--- tests/minunit.h
+++ tests/minunit.h
@@ -47,7 +47,7 @@
     message = test(); tests_run++; if (message) return message;
 
 #define RUN_TESTS(name) int main(int argc, char *argv[]) {\
-    argc = 1; \
+    (void)(argc);\
     debug("----- RUNNING: %s", argv[0]);\
     printf("-----\nRUNNING: %s\n", argv[0]);\
     char *result = name();\
