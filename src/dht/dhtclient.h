44a2a9ef3cf59eb690c48cc2f5a5a18ead16acf1
vagrant <vagrant@vagrantc.(none)>
2013-02-28 15:57:58 +0000

    indent

--- src/dht/dhtclient.h
+++ src/dht/dhtclient.h
@@ -5,11 +5,11 @@
 #include <dht/pendingresponses.h>
 
 typedef struct DhtClient {
-  DhtNode node;
-  DhtTable *table;
-  int socket;
-  HashmapPendingResponses *pending;
-  char *buf;
+    DhtNode node;
+    DhtTable *table;
+    int socket;
+    HashmapPendingResponses *pending;
+    char *buf;
 } DhtClient;
 
 DhtClient *DhtClient_Create(DhtHash id, uint32_t addr, uint16_t port);
