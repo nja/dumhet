ecd6082b564ed57eb4f897ce6680a2469471ae8e
Johan Andersson <nilsjohanandersson@gmail.com>
2013-06-19 12:26:57 +0000

    Commented close.h

--- src/dht/close.h
+++ src/dht/close.h
@@ -5,6 +5,7 @@
 #include <dht/hash.h>
 #include <dht/node.h>
 
+/* Holds the up to BUCKET_K closet nodes to id. */
 typedef struct CloseNodes
 {
     Hash id;
@@ -16,6 +17,8 @@ void CloseNodes_Destroy(CloseNodes *close);
 
 DArray *CloseNodes_GetNodes(CloseNodes *close);
 
+/* Adds node to the CloseNodes struct if there is still room or if node is
+ * closer than any of the present entries. */
 int CloseNodes_Add(CloseNodes *close, Node *node);
 
 #endif
