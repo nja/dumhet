dbce8e5e89e2c8cf7ef95fe42f3aa4e309f60f45
Johan Andersson <nilsjohanandersson@gmail.com>
2013-06-20 06:18:22 +0000

    Commented node.h

--- src/dht/node.h
+++ src/dht/node.h
@@ -19,13 +19,16 @@ Node *Node_Copy(Node *node);
 void Node_Destroy(Node *node);
 void Node_DestroyBlock(Node **node, size_t count);
 
+/* Nodes are the same when their id, addr and port are equals. */
 int Node_Same(Node *a, Node *b);
 
 int Node_DestroyOp(void *ignore, Node *node);
 
 typedef enum NodeStatus { Unknown, Good, Questionable, Bad } NodeStatus;
 
+/* Seconds until a node without queries or replies is questionable or bad. */
 #define NODE_RESPITE (15 * 60)
+/* After the respite, this many pending queries means the node is bad. */
 #define NODE_MAX_PENDING 2
 
 NodeStatus Node_Status(Node *node, time_t time);
