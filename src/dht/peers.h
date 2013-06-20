350ac277c374ee249980af0065ba35afc52caf84
Johan Andersson <nilsjohanandersson@gmail.com>
2013-06-20 06:37:17 +0000

    Commented peers.h

--- src/dht/peers.h
+++ src/dht/peers.h
@@ -9,11 +9,14 @@
 
 #define MAXPEERS 0x1F00
 
+/* Peers are BitTorrent clients. We don't talk to them, only search
+ * for them and track them. */
 typedef struct Peer {
     uint32_t addr;
     uint16_t port;
 } Peer;
 
+/* Stores announced peers for a single info_hash.  */
 typedef struct Peers {
     Hash info_hash;
     Hashmap *hashmap;
@@ -24,15 +27,22 @@ typedef struct Peers {
 Peers *Peers_Create(Hash *info_hash);
 void Peers_Destroy(Peers *peers);
 
+/* Pushes all peers on the result array. */
 int Peers_GetPeers(Peers *peers, DArray *result);
+/* Add the peer or update the time if already present. */
 int Peers_AddPeer(Peers *peers, Peer *peer);
 
+/* Clean out all peers added or updated before the cutoff time. */
 int Peers_Clean(Peers *peers, time_t cutoff);
 
 Hashmap *PeersHashmap_Create();
 void PeersHashmap_Destroy(Hashmap *hashmap);
 
+/* From a hashmap of Peers structs by info_hash, push all peers for the
+ * given info_hash on the result array. */
 int PeersHashmap_GetPeers(Hashmap *hashmap, Hash *info_hash, DArray *result);
+/* To a hashmap of Peers structs by info_hash, add the peer to the
+ * Peers of the info_hash, creating the Peers struct when needed. */
 int PeersHashmap_AddPeer(Hashmap *hashmap, Hash *info_hash, Peer *peer);
 
 #endif
