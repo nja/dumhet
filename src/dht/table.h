f68b2a21dfdcec5ef62ec9fb07f4525d81b5ae46
Johan Andersson <nilsjohanandersson@gmail.com>
2013-06-20 06:37:17 +0000

    Commented table.h

--- src/dht/table.h
+++ src/dht/table.h
@@ -8,6 +8,8 @@
 
 #define MAX_TABLE_BUCKETS (HASH_BITS + 1 - BUCKET_LAST_BITS)
 
+/* Holds buckets of nodes of decreasing distance to id.
+ * Nodes in bucket[i] share a prefix of i bits with id.*/
 typedef struct Table {
     Hash id;
     Bucket *buckets[MAX_TABLE_BUCKETS];
@@ -18,6 +20,7 @@ Table *Table_Create(Hash *id);
 void Table_Destroy(Table *table);
 void Table_DestroyNodes(Table *table);
 
+/* Calls op(context, node) for each node of the table. */
 int Table_ForEachNode(Table *table, void *context, NodeOp op);
 
 Bucket *Table_AddBucket(Table *table);
@@ -26,19 +29,31 @@ Bucket *Table_FindBucket(Table *table, Hash *id);
 Node *Table_FindNode(Table *table, Hash *id);
 
 enum Table_InsertNodeResultRc {
-    ERROR, OKAdded, OKReplaced, OKFull, OKAlreadyAdded
+    ERROR,                      /* Insert failed. */
+    OKAdded,                    /* Added to .bucket */
+    OKReplaced,                 /* Replaced .replaced in .bucket */
+    OKFull,                     /* Target bucket full. */
+    OKAlreadyAdded              /* Node of same id already added. */
 };
+
+/* Returns a new array with the BUCKET_K nodes from table that are
+ * closest to the id. Returns NULL on error. */
 DArray *Table_GatherClosest(Table *table, Hash *id);
 
 typedef struct Table_InsertNodeResult {
     enum Table_InsertNodeResultRc rc;
-    Bucket *bucket;
-    Node *replaced;
+    Bucket *bucket;             /* The target bucket (OKAdded,
+                                 * OKReplaced, OKAlreadyAdded). */
+    Node *replaced;             /* The replaced node (OKReplaced). */
 } Table_InsertNodeResult;
 
 Table_InsertNodeResult Table_InsertNode(Table *table, Node *node);
 
+/* Fins the node with the given id in the table and updates its
+ * reply_time.*/
 int Table_MarkReply(Table *table, Hash *id);
+/* Fins the node with the given id in the table and updates its
+ * query_time.*/
 void Table_MarkQuery(Table *table, Hash *id);
 
 bstring Table_Dump(Table *table);
