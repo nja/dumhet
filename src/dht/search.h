a363b41b2d10b0c065bab5178e53bfdb33544e02
Johan Andersson <nilsjohanandersson@gmail.com>
2013-06-20 06:37:17 +0000

    Commented search.h

--- src/dht/search.h
+++ src/dht/search.h
@@ -5,6 +5,13 @@
 #include <dht/peers.h>
 #include <dht/table.h>
 
+/* A search uses a Table and a Peers collection. It begins by adding
+ * to the search table copies of the nodes from a client's routing
+ * table. It then queries the search table nodes, adding any nodes and
+ * peers from the replies to the table and peers collection. New
+ * queries can be sent as long as there are nodes in the table that
+ * has yet to reply.  Both the table and the peers holds the target
+ * hash. */
 typedef struct Search {
     Table *table;
     Peers *peers;
@@ -13,13 +20,16 @@ typedef struct Search {
 Search *Search_Create(Hash *id);
 void Search_Destroy(Search *search);
 
+/* Copy the nodes from source and insert them to the search table. */
 int Search_CopyTable(Search *search, Table *source);
 
 #define SEARCH_RESPITE 3
 #define SEARCH_MAX_PENDING 2
 
+/* Finds nodes in the search table that should be queried. */
 int Search_NodesToQuery(Search *search, DArray *nodes, time_t time);
 
+/* Adds to the collection of found peers by the search. */
 int Search_AddPeers(Search *search, Peer *peers, int count);
 
 #endif
